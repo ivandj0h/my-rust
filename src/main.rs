bc7c70ccc81f13c080019bf4b16e953a2dcced78
ivandjoh <ivandi.djoh@gmail.com>
2022-11-24 23:04:43 +0800

    if statements

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,23 @@ fn main() {
     //}
 
     // While loop
-    let mut count = 0;
+    //let mut count = 0;
 
-    while count <= 10 {
-        println!("Count: {}", count);
-        count += 1;
+    //while count <= 10 {
+    //println!("Count: {}", count);
+    //  count += 1;
+    //}
+
+    // If Statement
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
     }
 }
