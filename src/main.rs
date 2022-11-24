d7a2f861f69f3420aee61d8c6434aaf12942a74e
ivandjoh <ivandi.djoh@gmail.com>
2022-11-24 22:49:12 +0800

    for loop

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
-    let num = 5;
-    match num {
-        1 => println!("One"),
-        2 => println!("Two"),
-        3 => println!("Three"),
-        4 => println!("Four"),
-        5 => println!("Five"),
-        _ => println!("Something else"),
+    //Match function
+    //let num = 5;
+    //match num {
+    //  1 => println!("One"),
+    //  2 => println!("Two"),
+    //  3 => println!("Three"),
+    //  4 => println!("Four"),
+    //  5 => println!("Five"),
+    //  _ => println!("Something else"),
+    //}
+
+    // For loop
+    for n in 1..100 {
+        print!("{} ", n);
     }
 }
