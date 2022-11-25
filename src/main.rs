6de71b7e8e0709182ef86fd07cb8b8fcad0fec3e
ivandjoh <ivandi.djoh@gmail.com>
2022-11-25 10:06:59 +0800

    new updates

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![allow(unused)]
+
+use ::std::cmp::Ordering;
+use rand::Rng;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+fn main() {
+    println!("Hello, world!");
+}
